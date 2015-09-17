91a6beeae7a4e800a76a4906a1b9160914dfdca3
etmarch <etmarch@gmail.com>
2015-09-17 05:11:02 -0400

    Tons of testing and playing around with Material-UI

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,7 +10,12 @@ appBarClock = new Chronos.Timer(1000); // initializing global app clock
 
 let iconList = ["add_circle", "view_list", "attach_money"]; // list of icons that will be implemented
 // Material-UI Componenets
-const {RaisedButton, Checkbox, ListItem, List, AppCanvas, AppBar, Card, IconButton, LeftNav, Toggle, FontIcon} = MUI;
+const {RaisedButton, FlatButton, Checkbox, Toolbar, ToolbarGroup, ToolbarTitle, ToolbarSeparator,
+    DropDownMenu, ListItem, List, ListDivider, AppCanvas, AppBar, Avatar, Card, Dialog, IconButton, LeftNav, Toggle, FontIcon, Styles} = MUI;
+
+// Reference Styles.Colors global variable
+const Colors = MUI.Styles.Colors;
+
 // Init the material-ui framework
 const ThemeManager = new MUI.Styles.ThemeManager();
 
@@ -48,10 +53,32 @@ App = React.createClass({
     }
   },
 
+  _handleCustomDialogTouchTap() {
+    this.refs.customDialog.show();
+  },
+
+
   renderContracts() {
+
+    //Testing modal Standard Actions
+    let standardActions = [
+      { text: 'Cancel' },
+      { text: 'Submit', onTouchTap: this._onDialogSubmit, ref: 'submit' }
+    ];
+
     // Get contracts from this.data.contracts
     return this.data.contracts.map((contract) => {
-      return <Contract key={contract._id} contract={contract} />;
+      //return <Contract key={contract._id} contract={contract} />;
+      return <div><ListItem
+          key={contract._id}
+          primaryText={contract.text}
+          initiallyOpen={true}
+          leftCheckbox={<Checkbox name="checky" /> }
+          secondaryText="Hey you how are you man hey you how are yu mna hey yu how are yu man"
+          rightAvatar={<Avatar color={Colors.deepOrange300} backgroundColor={Colors.red300}></Avatar>} />
+          <ListDivider inset={false} />
+      </div>
+
     });
   },
 
@@ -61,6 +88,7 @@ App = React.createClass({
     });
   },
 
+  // Shows / hides the side menu
   toggleLeftNav() {
     this.refs.leftNav.toggle();
   },
@@ -76,55 +104,90 @@ App = React.createClass({
     console.log("success? - "+okay);
   },
 
+  // Handle clicks on links on the left menu
+  leftNavChange(e, selectedIndex, menuItem) {
+    Utils.cl(e.target+"  Menu Item Object: "+JSON.stringify(menuItem));
+    FlowRouter.go(menuItem.route);
+  },
 
   render() {
 
+    // Sample Code for Links in the left hidden nav menu
     let menuItems = [
-      { route: 'get-started', text: 'Get Started' },
-      { route: 'customization', text: 'Customization' },
-      { route: 'components', text: 'Components' }
+      { route: '/', text: 'New Contract' },
+      { route: '/contracts/', text: 'Contracts' },
+      { route: 'expensesList', text: 'Expenses' }
     ];
 
+    // Sample code for menu items in a toolbar Toolbar
+    let filterOptions = [
+      { payload: '1', text: 'All Broadcasts' },
+      { payload: '2', text: 'All Voice' },
+      { payload: '3', text: 'All Text' },
+      { payload: '4', text: 'Complete Voice' },
+      { payload: '5', text: 'Complete Text' },
+      { payload: '6', text: 'Active Voice' },
+      { payload: '7', text: 'Active Text' }
+    ];
+
+    // Toggle button overwrite
     let toggleStyle = {
       display: 'inline-block', width: 'auto'
     }
 
     return (
         <AppCanvas className="">
-          <AppBar title="Studio Marchand Contract Management Center" onLeftIconButtonTouchTap={this.toggleLeftNav}>
-            <div>TimeOffset: {TimeSync.serverOffset()}   |||   {this.data.currentTime.toString()}</div>
+
+          {/* ToDo: Migrate this into its own component  --   <AppHeader /> for example */}
+          <AppBar title="Studio Marchand Contract Management Center" onLeftIconButtonTouchTap={this.toggleLeftNav} className="container-fluid">
+            <div>TimeOffset: {TimeSync.serverOffset()}   <ToolbarSeparator/>   {moment(this.data.currentTime).format('hh:mm A L')}</div>
 
             <AccountsUIWrapper />
 
-              <Toggle
-                  style={toggleStyle}
-                  isToggled={this.state.liveContract}
-                  onToggle={this.toggleIsLiveContract} />
+            <Toggle
+                style={toggleStyle}
+                isToggled={this.state.liveContract}
+                onToggle={this.toggleIsLiveContract} />
 
-              <LiveLight
-                  isLiveContract={this.state.liveContract} />
+            <LiveLight
+                isLiveContract={this.state.liveContract} />
 
-            <IconButton iconClassName="material-icons" tooltipPosition="bottom-center"
-                        tooltip="Done">report_problem</IconButton>
-
+            <IconButton iconClassName="material-icons" tooltipPosition="bottom-center" tooltip="Done">report_problem</IconButton>
           </AppBar>
+
+
+
           <div className="container">
-          <div className="blocky"></div>
+            <div className="blocky"></div>
 
 
-            <LeftNav ref="leftNav" docked={false} menuItems={menuItems} />
+            <Toolbar>
+              <ToolbarGroup key={0} float="left">
+                <DropDownMenu menuItems={filterOptions} />
+                <ToolbarSeparator/>
+                <ToolbarTitle text="SAMPLE HEADER" />
+              </ToolbarGroup>
+            </Toolbar>
 
-              <form className="new-contract" onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    ref="textInput"
-                    placeholder="Type to add new tasks"/>
-              </form>
+            <LeftNav
+                ref="leftNav"
+                docked={false}
+                menuItems={menuItems}
+                onChange={this.leftNavChange} />
 
-          <div className="row">
-            {this.renderContracts()}
+            <form className="new-contract" onSubmit={this.handleSubmit}>
+              <input
+                  type="text"
+                  ref="textInput"
+                  placeholder="Type to add new tasks"/>
+            </form>
+
+            <div className="row">
+              <List subheader="Today">
+                {this.renderContracts()}
+              </List>
+            </div>
           </div>
-        </div>
         </AppCanvas>
     );
   }
@@ -140,7 +203,7 @@ LiveLight = React.createClass({
     const lightClassName = this.props.isLiveContract ? "live-light active" : "live-light ";
 
     return (
-          <div className={lightClassName} />
+        <div className={lightClassName} />
     );
   }
 });
